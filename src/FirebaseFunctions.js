2b9df5765a0cb6a1025b1f723da8c500e0103d71
Zen <amaar5ali@gmail.com>
2023-07-16 00:39:58 -0400

    made Firebase dynamic function to grab data that was submitted when created by unique user+Daily/Weekly rirTarget state & workoutcomplete button+added security rules to Firestore database that allows user to edit and see their own specific data unique to their UID from Auth

---
+++ src/FirebaseFunctions.js
@@ -0,0 +1,10 @@
+import { db } from './firebase';
+
+export const addMesocycle = async (userId, mesocycle) => {
+    try {
+        const docRef = await db.collection('users').doc(userId).collection('mesocycles').add(mesocycle);
+        console.log("Document written with ID: ", docRef.id);
+    } catch (error) {
+        console.error("Error adding document: ", error);
+    }
+}
--- src/WorkoutForm.js
+++ src/WorkoutForm.js
@@ -1,10 +1,11 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./WorkoutForm.module.css"
 import MesocycleForm from "./MesocycleForm";
 import DayForm from "./DayForm";
 import MuscleGroupForm from "./MuscleGroupForm";
 import ExerciseForm from "./ExerciseForm";
 import SessionForm from "./SessionForm";
+import { addMesocycle } from "./FirebaseFunctions";
 
 const WorkoutForm = () => {
     const [mesoLength, setMesoLength] = useState("");
@@ -18,10 +19,46 @@ const WorkoutForm = () => {
     const [reps, setReps] = useState("");
     const [rirTarget, setRirTarget] = useState("");
     const [youtubeVideoId, setYoutubeVideoId] = useState("");
+    const [currentWeek, setCurrentWeek] = useState(1);
+    const [currentDay, setCurrentDay] = useState(1);
+
+    useEffect(() => {
+        // Calculate rirTarget based on currentWeek and mesoLength
+        const calculateRirTarget = (currentWeek, mesoLength) => {
+            if (currentWeek === mesoLength) {
+                // Last week of meso, deload
+                return 8;
+            } else {
+                // Other weeks of meso
+                return mesoLength - currentWeek - 1;
+            }
+        }
+
+        setRirTarget(calculateRirTarget(currentWeek, mesoLength));
+    }, [currentWeek, mesoLength]);
+
+    const handleWorkoutCompletion = () => {
+        // Increment currentDay
+        setCurrentDay(currentDay + 1);
+
+        // If currentDay exceeds daysPerWeek, reset currentDay and increment currentWeek
+        if (currentDay >= daysPerWeek) {
+            setCurrentDay(1);
+            setCurrentWeek(currentWeek + 1);
+        }
+    }
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        // TODO : Submit the form data to Firebase
+        // Prepare the mesocycle data
+        const mesocycle = {
+            mesoLength,
+            daysPerWeek,
+            // add other properties that might need to be created/set during meso creation by user
+        };
+
+        // Submit meso data to Firebase
+        addMesocycle(userId, mesocycle);
 
         // Clear the form fields
         setMesoLength("");
@@ -44,6 +81,7 @@ const WorkoutForm = () => {
             <MuscleGroupForm muscleGroup={muscleGroup} setMuscleGroup={setMuscleGroup} exerciseName={exerciseName} setExerciseName={setExerciseName} />
             <ExerciseForm exerciseName={exerciseName} exerciseType={exerciseType} setExerciseType={setExerciseType} youtubeVideoId={youtubeVideoId} setYoutubeVideoId={setYoutubeVideoId} />
             <SessionForm exerciseName={exerciseName} sets={sets} setSets={setSets} weight={weight} setWeight={setWeight} reps={reps} setReps={setReps} rirTarget={rirTarget} setRirTarget={setRirTarget} />
+            <button type="button" onClick={handleWorkoutCompletion}>Complete Workout</button>
             <button type="submit">Create Mesocycle</button>
         </form>
     );
