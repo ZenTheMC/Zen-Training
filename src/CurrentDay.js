ca9a1afdbf4244610669a7b238732736529bc2b6
Zen <amaar5ali@gmail.com>
2023-08-26 01:36:03 -0400

    still debugging add/remove set

--- src/CurrentDay.js
+++ src/CurrentDay.js
@@ -25,21 +25,21 @@ const CurrentDay = ({ userId }) => {
   // Fetch mesocycle data from Firebase
   const fetchMesocycleData = async () => {
     try {
-        const mesocycles = await getMesocycles(userId);
-        if (mesocycles && mesocycles.length > 0) {
-            // Sort by completion status and creation time
-            mesocycles.sort((a, b) => {
-                if (a.completed && !b.completed) return 1;
-                if (!a.completed && b.completed) return -1;
-                return b.createdAt.seconds - a.createdAt.seconds;  // Most recent comes first
-            });
+      const mesocycles = await getMesocycles(userId);
+      if (mesocycles && mesocycles.length > 0) {
+        // Sort by completion status and creation time
+        mesocycles.sort((a, b) => {
+          if (a.completed && !b.completed) return 1;
+          if (!a.completed && b.completed) return -1;
+          return b.createdAt.seconds - a.createdAt.seconds;  // Most recent comes first
+        });
 
-            setMesocycle(mesocycles[0]);  // Fetch the first mesocycle based on sorted conditions
-        } else {
-            console.log("No mesocycles found for the user!");
-        }
+        setMesocycle(mesocycles[0]);  // Fetch the first mesocycle based on sorted conditions
+      } else {
+        console.log("No mesocycles found for the user!");
+      }
     } catch (error) {
-        console.error("Error fetching mesocycle data:", error);
+      console.error("Error fetching mesocycle data:", error);
     }
   };
 
@@ -52,9 +52,9 @@ const CurrentDay = ({ userId }) => {
 
     // Check if it's the last day of the last week
     if (currentWeek === mesocycle.weeks && currentDay === mesocycle.days[mesocycle.days.length - 1].dayOfWeek) {
-        updateMesocycleCompletionStatus(userId, mesocycleId);  // This function will update the status in Firebase
+      updateMesocycleCompletionStatus(userId, mesocycleId);  // This function will update the status in Firebase
     } else {
-        // Logic to proceed to the next day or week
+      // Logic to proceed to the next day or week
     }
   };
 
@@ -64,21 +64,18 @@ const CurrentDay = ({ userId }) => {
   };
 
   const addSet = (exerciseName) => {
-    setExerciseSets(prevState => ({
-      ...prevState,
-      [exerciseName]: [...(prevState[exerciseName] || [{ weight: "", reps: "" }]), { weight: "", reps: "" }]
-    }));
+    console.log('addSet is called');
+    const newSet = { weight: "", reps: "" };
+    const updatedSets = exerciseSets[exerciseName] ? [...exerciseSets[exerciseName], newSet] : [newSet];
+    setExerciseSets({ ...exerciseSets, [exerciseName]: updatedSets });
+    console.log("Updated sets:", updatedSets);
   };
-
+  
   const removeSet = (exerciseName) => {
-    setExerciseSets(prevState => {
-      const updatedSets = [...prevState[exerciseName]];
-      updatedSets.pop();
-      return {
-        ...prevState,
-        [exerciseName]: updatedSets
-      };
-    });
+    console.log('removeSet is called');
+    const updatedSets = exerciseSets[exerciseName] ? exerciseSets[exerciseName].slice(0, -1) : [];
+    setExerciseSets({ ...exerciseSets, [exerciseName]: updatedSets });
+    console.log("Updated sets after removal:", updatedSets);
   };
 
   const handleSetChange = (exerciseName, setIndex, field, value) => {
@@ -92,18 +89,26 @@ const CurrentDay = ({ userId }) => {
     });
   };
 
-  const showOptionsForExercises = (exerciseName) => {
+  const showOptionsForExercises = (event, exerciseName) => {
+    event.stopPropagation();
     setShownOptions(exerciseName);
+    document.addEventListener('click', hideOptions);
   };
-
+  
   const hideOptions = () => {
     setShownOptions(null);
+    document.removeEventListener('click', hideOptions);
   };
+
+  useEffect(() => {
+    console.log("exerciseSets has changed:", exerciseSets);
+  }, [exerciseSets]);
 
   const currentDayExercises = mesocycle.days.find(day => day.dayOfWeek === currentDay);
 
   return (
     <div className={styles.CurrentDay}>
+      {console.log('exerciseSets in render:', exerciseSets)}
       <MesoInfo name={mesocycle.name} currentWeek={currentWeek} currentDay={currentDay} />
       <Calendar
         weeks={mesocycle.weeks}
@@ -115,10 +120,10 @@ const CurrentDay = ({ userId }) => {
         <div className={styles.Exercise} key={index} onBlur={hideOptions} tabIndex={0}>
           <div className={`${styles.ExerciseName} ${styles.ExerciseNameContainer}`}>
             {exercise.name}
-            <span className={styles.Ellipsis} onClick={() => showOptionsForExercises(exercise.name)}>...</span>
+            <span className={styles.Ellipsis} onClick={(e) => showOptionsForExercises(e, exercise.name)}>...</span>
             <div className={`${styles.Options} ${shownOptions === exercise.name ? styles.showOptions : ""}`}>
-              <button onClick={() => addSet(exercise.name)}>Add Set</button>
-              <button onClick={() => removeSet(exercise.name)}>Remove Set</button>
+              <button onClick={(e) => { e.stopPropagation(); addSet(exercise.name); }}>Add Set</button>
+              <button onClick={(e) => { e.stopPropagation(); removeSet(exercise.name); }}>Remove Set</button>
             </div>
           </div>
           <p className={styles.MuscleGroup}>{exercise.muscleGroup}</p>
@@ -127,7 +132,7 @@ const CurrentDay = ({ userId }) => {
             <div key={setIndex}>
               <input placeholder="Weight" value={set.weight} onChange={(e) => handleSetChange(exercise.name, setIndex, "weight", e.target.value)} />
               <input placeholder="Reps" value={set.reps} onChange={(e) => handleSetChange(exercise.name, setIndex, "reps", e.target.value)} />
-              </div>
+            </div>
           ))}
           {/* Add a button or mechanism to save this data */}
         </div>
