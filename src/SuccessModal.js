9d15f8851de0560ac670d624bf77b744ec52344b
Zen <amaar5ali@gmail.com>
2023-09-30 00:18:20 -0400

    signin/up/successmodal media mobile query

--- src/SuccessModal.js
+++ src/SuccessModal.js
@@ -2,13 +2,13 @@ import React from 'react';
 import styles from './SuccessModal.module.css';
 import { Link } from 'react-router-dom';
 
-const SuccessModal = ({ show, onClose, children }) => {
+const SuccessModal = ({ show, onClose,}) => {
     if (!show) return null;
 
     return (
         <div className={styles.overlay}>
             <div className={styles.modal}>
-                <h3>Account created successfully! You can now <Link to="/signin">sign in</Link></h3>
+                <h3>Account created successfully! You can now <Link className={styles.link} to="/signin">sign in</Link></h3>
                 <button onClick={onClose}>Close</button>
             </div>
         </div>
