7fcbac74c516ad329631c408b0e81ca7f7baff03
Zen <amaar5ali@gmail.com>
2023-09-30 22:19:27 -0400

    fixed border not rendering on mobile view calendar with state in calendar component, and also converted all pixels to ems

--- src/Calendar.js
+++ src/Calendar.js
@@ -1,7 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './Calendar.module.css';
 
 const Calendar = ({ weeks, days, onSelectDay }) => {
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 576);
+
+  useEffect(() => {
+      const handleResize = () => {
+          setIsMobile(window.innerWidth <= 576);
+      };
+
+      window.addEventListener('resize', handleResize);
+      return () => {
+          window.removeEventListener('resize', handleResize);
+      };
+  }, []);
+
+  const getDayLabel = (dayOfWeek) => {
+      if (!isMobile) return dayOfWeek;
+      const abbreviated = {
+          'Monday': 'Mon',
+          'Tuesday': 'Tue',
+          'Wednesday': 'Wed',
+          'Thursday': 'Thu',
+          'Friday': 'Fri',
+          'Saturday': 'Sat',
+          'Sunday': 'Sun'
+      };
+      return abbreviated[dayOfWeek] || dayOfWeek;
+  };
+
   return (
     <div className={styles.Calendar}>
       <table>
@@ -22,7 +49,7 @@ const Calendar = ({ weeks, days, onSelectDay }) => {
                   onClick={() => onSelectDay(weekIndex + 1, days[weekIndex][dayIndex])}
                   data-day={days[weekIndex][dayIndex]?.dayOfWeek}
                 >
-                  {days[weekIndex][dayIndex]?.dayOfWeek}
+                  {getDayLabel(days[weekIndex][dayIndex]?.dayOfWeek)}
                 </td>
               ))}
             </tr>
