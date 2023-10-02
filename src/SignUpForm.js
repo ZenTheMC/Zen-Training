610aaeb07ea178ab66c9b346a8176f5cb788fcce
Zen <amaar5ali@gmail.com>
2023-10-02 00:09:15 -0400

    added email regex validation + custom messages for successmodal for validation and kept the successful account creation too, now re-usable

--- src/SignUpForm.js
+++ src/SignUpForm.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { auth } from "./Firebase";
 import { Link } from "react-router-dom";
-import styles from "./SignUpForm.module.css"
+import styles from "./SignUpForm.module.css";
 import SuccessModal from "./SuccessModal";
 
 const SignUpForm = () => {
@@ -10,12 +10,24 @@ const SignUpForm = () => {
     const [password, setPassword] = useState('');
     const [error, setError] = useState(null);
     const [showModal, setShowModal] = useState(false);
+    const [modalMessage, setModalMessage] = useState('');
+
+    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
 
     const handleSubmit = async (event) => {
         event.preventDefault();
+
+        // Check for valid email using regex
+        if (!emailRegex.test(email)) {
+            setModalMessage("Please enter a valid email address.");
+            setShowModal(true);
+            return;
+        }
+
         try {
             await createUserWithEmailAndPassword(auth, email, password);
             setShowModal(true);
+            setModalMessage("Successfully signed up! Welcome to the Hypertrophy App!");
         } catch (error) {
             setError(error.message);
         }
@@ -24,8 +36,10 @@ const SignUpForm = () => {
     return (
         <div className={styles.container}>
             <SuccessModal
-            show={showModal}
-            onClose={() => setShowModal(false)}>
+                show={showModal}
+                onClose={() => setShowModal(false)}
+            >
+                {modalMessage}
             </SuccessModal>
             <h1>Welcome to the Hypertrophy App!</h1>
             <p>This app is for anyone wanting to maximize their muscle building potential</p>
--- src/SuccessModal.js
+++ src/SuccessModal.js
@@ -2,13 +2,14 @@ import React from 'react';
 import styles from './SuccessModal.module.css';
 import { Link } from 'react-router-dom';
 
-const SuccessModal = ({ show, onClose,}) => {
+const SuccessModal = ({ show, onClose, children, showLink = true }) => {
     if (!show) return null;
 
     return (
         <div className={styles.overlay}>
             <div className={styles.modal}>
-                <h3>Account created successfully! You can now <Link className={styles.link} to="/signin">sign in</Link></h3>
+                {children}
+                {showLink && <h3>You can now <Link className={styles.link} to="/signin">sign in</Link></h3>}
                 <button onClick={onClose}>Close</button>
             </div>
         </div>
